04559e90e0c92e1a3adf3b0c7131a98aa8fe14e0
saaaucorp <sabbir.azim@springrain.io>
2022-08-26 15:42:12 +0600

    node core module

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
+//dependencies
 const http = require("http");
- const fs = require("fs");
-const port = 8081;
-const server = http.createServer(function (req, res) {
-  
+const url = require("url");
+const fs = require("fs");
+// module scaffoldings
+const app = {};
+// configuration variables
+app.createServer = () => {
+  const server = http.createServer(app.handleReqRes);
+  server.listen(3000, () => {
+    console.log("Listening on port 3000");
+  });
+};
 
-  res.end("server start at http://localhost:" + port);
- 
-  
-});
-
-server.listen(port);
-console.log("server listening on " + port + " port");
+//handler createServer
+app.handleReqRes = (req, res) => {
+  // request handler
+  const parsedUrl = url.parse(req.url, true);
+  console.log(parsedUrl);
+  const path = parsedUrl.path;
+  console.log(path);
+  const triemPath = path.replace(/^\/+|\/+$/g, "");
+  console.log(triemPath);
+  const method = req.method.toLowerCase();
+  console.log(method);
+  const queryStringObject = parsedUrl.query;
+  console.log(queryStringObject);
+  //response handler
+  res.end("sabbir al azim shehab");
+};
+app.createServer();
